9a918de9a3c4fca759433ec837783a8106d16384
Harry067 <uggies10@gmail.com>
2021-01-26 08:34:03 +1000

    aa

---
+++ app.js
@@ -0,0 +1,5 @@
+var jbBtn = document.createElement('button');
+var jbBtnText = document.createTextNode('Click');
+jbBtn.appendChild(jbBtnText);
+document.body.appendChild(jbBtn);
+console.log('btn added');
